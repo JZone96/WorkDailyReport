e2be436926c0bd3d07642f76bad18531e0f647b8
JZone96 <jason.liberti@live.it>
2026-01-13 22:35:05 +0100

    Creato gitignore

--- Program.cs
+++ Program.cs
@@ -3,6 +3,7 @@ using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
 using Microsoft.Extensions.Options;
+using System.Text;
 using WorkDailyReport.ActivityWatch;
 using WorkDailyReport.Config;
 using WorkDailyReport.ETL;
